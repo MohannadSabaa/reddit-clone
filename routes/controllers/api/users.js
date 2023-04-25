1fb3c367b80707ac8776c14da1fff1192c2e463c
MohannadSabaa <Moh4nn4d@gmail.com>
2023-04-25 20:56:50 +0300

    fix register bug

--- routes/controllers/api/users.js
+++ routes/controllers/api/users.js
@@ -1,5 +1,4 @@
 const bcrypt = require('bcrypt');
-const Joi = require('joi');
 const {signToken} = require('../../../utils/token');
 const connection = require('../../../model/database');
 const schema = require('../../../utils/joi');
@@ -10,20 +9,15 @@ const getUsers = async (req, res) => {
     res.status(201).json(users);
 }
 const addNewUser = async(req, res) => {
-try {
+
     const {name, email, password} = req.body;
-    const validate = await schema.validateAsync(schema, req.body);
+  
     const hashedPass = await bcrypt.hash(password, 10);
     const user = await connection.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3)',[name, email, hashedPass]);
     const result = await user.rows;
     const token = await signToken({name, email});
-    res.status(200).cookie('token', token).redirect('/');
-}
 
-catch (error) {
-res.status(404).json(error)
-}
-
+res.status(200).cookie('token', token).redirect('/');
 }
 
 module.exports = {addNewUser, getUsers};
